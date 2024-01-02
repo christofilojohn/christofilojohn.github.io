61c67f67907fe221cb18651377658a8570d6bff8
Ioannis Christofilogiannis <ichristofilogian@tuc.gr>
2024-01-02 02:34:03 +0200

    push new
    updated navbar new photos

--- navbar.js
+++ navbar.js
@@ -1,5 +1,6 @@
 // Function to load the navbar
 document.addEventListener("DOMContentLoaded", function() {
+    currentSlide(1);
     var navbarPlaceholder = document.getElementById('navbar-placeholder');
     if (navbarPlaceholder) {
         var xhr = new XMLHttpRequest();
@@ -13,22 +14,23 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-// Function to toggle the menu in mobile view
-function toggleMenu() {
-    var navItems = document.getElementById('navItems');
-    if (navItems.style.display === 'block') {
-        navItems.style.display = 'none';
-    } else {
-        navItems.style.display = 'block';
+
+function currentSlide(n) {
+    var i;
+    var slides = document.getElementsByClassName("image-container");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { n = 1; }
+    if (n < 1) { n = slides.length; }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[n - 1].style.display = "block"; // Changed from flex to block
+    dots[n - 1].className += " active";
 }
 
-// Reset navbar when window is resized
-window.onresize = function() {
-    var navItems = document.getElementById('navItems');
-    if (window.innerWidth > 768) { // Adjust this value based on your media query breakpoint
-        navItems.style.display = 'flex'; // Or 'block', depending on your original design
-    } else {
-        navItems.style.display = 'none';
-    }
-};
